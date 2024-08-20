16a04694c75f610766583dd9c0897634f7789f87
ejshafran <ejshafran@hilma.tech>
2024-08-21 01:44:40 +0300

    refactor: rename variables

--- lua/delog/init.lua
+++ lua/delog/init.lua
@@ -2,20 +2,20 @@ local delog = {}
 
 function delog.delog()
   local cursor = vim.api.nvim_win_get_cursor(0)
-  local row = cursor[1]
-  local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
-  local pad = line:match "^%s*"
+  local lnum = cursor[1]
+  local line = vim.api.nvim_buf_get_lines(0, lnum, lnum + 1, false)[1]
+  local padding = line:match "^%s*"
   -- TODO: configure where to get this from
-  local reg = vim.fn.getreg "0"
+  local register = vim.fn.getreg "0"
   ---@type string
   -- TODO: default format
-  local fmt = vim.b.fmtstring or "%s"
+  local format = vim.b.fmtstring or "%s"
   -- TODO: repeat as much as possible
-  local msg = fmt:format(reg, reg)
+  local message = format:format(register, register)
   -- TODO: deal with multiline strings
-  assert(msg:match "\n" == nil, "TODO: multiline strings")
-  local result = { pad .. msg }
-  vim.api.nvim_buf_set_lines(0, row, row, false, result)
+  assert(message:match "\n" == nil, "TODO: multiline strings")
+  local result = { padding .. message }
+  vim.api.nvim_buf_set_lines(0, lnum, lnum, false, result)
 end
 
 return delog
