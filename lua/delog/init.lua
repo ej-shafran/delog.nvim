6013156e65f5551f87312b8ad7a5ab47c5d05df6
ejshafran <ejshafran@hilma.tech>
2024-08-21 00:55:16 +0300

    feat: read from register

--- lua/delog/init.lua
+++ lua/delog/init.lua
@@ -3,10 +3,12 @@ local delog = {}
 function delog.delog()
   local cursor = vim.api.nvim_win_get_cursor(0)
   local row = cursor[1]
+  local reg = vim.fn.getreg "0"
   -- TODO: default format
   local fmt = vim.b.fmtstring or "%s"
+  local result = vim.split(fmt:format(reg), "\n")
   -- TODO: respect whitespace
-  vim.api.nvim_buf_set_lines(0, row, row, false, { fmt:format "hello" })
+  vim.api.nvim_buf_set_lines(0, row, row, false, result)
 end
 
 return delog
