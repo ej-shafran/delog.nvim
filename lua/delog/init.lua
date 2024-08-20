fe5f36052c9a21901e69a4b95e39a9139b30bc1e
ejshafran <ejshafran@hilma.tech>
2024-08-21 00:51:51 +0300

    feat: read from `vim.b.fmtstring`

--- lua/delog/init.lua
+++ lua/delog/init.lua
@@ -3,7 +3,10 @@ local delog = {}
 function delog.delog()
   local cursor = vim.api.nvim_win_get_cursor(0)
   local row = cursor[1]
-  vim.api.nvim_buf_set_lines(0, row, row, false, { "Hello from delog" })
+  -- TODO: default format
+  local fmt = vim.b.fmtstring or "%s"
+  -- TODO: respect whitespace
+  vim.api.nvim_buf_set_lines(0, row, row, false, { fmt:format "hello" })
 end
 
 return delog
